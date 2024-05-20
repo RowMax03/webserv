a4ebbebd2d5ce1456b65190359f77ec23d1fe11c
nscheefe <nils.scheefe@gmail.com>
2024-05-20 19:06:26 +0200

    [conf_parser] Add inital files

---
+++ include/conf_parser.hpp
@@ -0,0 +1,12 @@
+#ifndef WEBSERV_CONFPARSER_H
+#define WEBSERV_CONFPARSER_H
+
+namespace confParser {
+
+    class confParser {
+
+    };
+
+} // confParser
+
+#endif //WEBSERV_CONFPARSER_H
---
+++ srcs/conf_parser/conf_parser.cpp
@@ -0,0 +1,8 @@
+//
+// Created by nils on 5/20/24.
+//
+
+#include "../../include/conf_parser.hpp"
+
+namespace confParser {
+} // confParser
