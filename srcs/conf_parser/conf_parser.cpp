6ecef6eebe80c3c5b06d50d389e13f2bc95e83db
nscheefe <nils.scheefe@gmail.com>
2024-05-20 23:00:06 +0200

    [conf_parser] Update conf_parser.cpp add default constructor for struct member variables and Update constructor functions

--- srcs/conf_parser/conf_parser.cpp
+++ srcs/conf_parser/conf_parser.cpp
@@ -4,30 +4,36 @@
 namespace confParser {
 
     LocationConfig::LocationConfig()
-            : path(""),
-              methods({""}),
-              redirect_status(0),
-              redirect_url(""),
-              root(""),
-              autoindex(false),
-              index(""),
-              next(NULL)
-    {}
+            : path(""), redirect_status(0), redirect_url(""), root(""),
+              autoindex(false), index(""), next(NULL)
+    {
+        methods[0] = "";
+        methods[1] = "";
+        methods[2] = "";
+    }
 
     ServerConfig::ServerConfig()
-            : listen(0),
-              server_name(""),
-              server_names_hash_bucket_size(0),
-              error_page(std::make_pair(0, "")),
-              client_max_body_size(""),
-              locations(NULL)
+            : listen(0), server_name(""), server_names_hash_bucket_size(0),
+              error_pages(NULL), client_max_body_size(""), locations(NULL)
     {}
 
-    confParser::confParser(const std::string& configFile) : serverConfig(new ServerConfig) {
+    confParser::confParser(const std::string& configFile)
+            : serverConfig(new ServerConfig)
+    {
         parseConfigFile(configFile);
     }
 
-    confParser::confParser(const confParser& other) : serverConfig(new ServerConfig(*other.serverConfig)) {}
+    confParser::confParser(const confParser& other)
+            : serverConfig(new ServerConfig(*other.serverConfig))
+    {}
+
+    confParser& confParser::operator=(const confParser& other) {
+        if (this != &other) {
+            delete serverConfig;
+            serverConfig = new ServerConfig(*other.serverConfig);
+        }
+        return *this;
+    }
 
     confParser::~confParser() {
         delete serverConfig;
