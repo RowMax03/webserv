55a9a7ad920a492641703eb772796f355adb15e8
nscheefe <nils.scheefe@gmail.com>
2024-05-20 20:57:22 +0200

    [conf_parser] Update headfile and cpp file add basic function structur, canonical form and structs

--- include/conf_parser.hpp
+++ include/conf_parser.hpp
@@ -1,12 +1,48 @@
-#ifndef WEBSERV_CONFPARSER_H
-#define WEBSERV_CONFPARSER_H
+#pragma once
 
+#include <string>
 namespace confParser {
 
-    class confParser {
+    struct LocationConfig {
+        std::string path;
+        std::string methods;
+        int redirect_status;
+        std::string redirect_url;
+        std::string root;
+        bool autoindex;
+        std::string index;
+        LocationConfig* next;
+    };
 
+    struct ServerConfig {
+        int listen;
+        std::string server_name;
+        int server_names_hash_bucket_size;
+        std::pair<int, std::string> error_page;
+        std::string client_max_body_size;
+        LocationConfig* locations;
+    };
+
+    class confParser {
+    public:
+        // Constructor that takes a configuration file
+        confParser(const std::string& configFile);
+
+        // Copy constructor
+        confParser(const confParser& other);
+
+        // Destructor
+        ~confParser();
+
+        // ServerConfig member variable
+        ServerConfig* serverConfig;
+
+    private:
+        // Method to parse the configuration file
+        void parseConfigFile(const std::string& configFile);
+        void readConfigFile(int fd);
+        void setServerConfigValue();
+        void setLocationConfigValue();
     };
 
 } // confParser
-
-#endif //WEBSERV_CONFPARSER_H
--- srcs/conf_parser/conf_parser.cpp
+++ srcs/conf_parser/conf_parser.cpp
@@ -1,8 +1,50 @@
-//
-// Created by nils on 5/20/24.
-//
 
 #include "../../include/conf_parser.hpp"
 
 namespace confParser {
+
+    LocationConfig::LocationConfig()
+            : path(""),
+              methods(""),
+              redirect_status(0),
+              redirect_url(""),
+              root(""),
+              autoindex(false),
+              index(""),
+              next(NULL)
+    {}
+
+    ServerConfig::ServerConfig()
+            : listen(0),
+              server_name(""),
+              server_names_hash_bucket_size(0),
+              error_page(std::make_pair(0, "")),
+              client_max_body_size(""),
+              locations(NULL)
+    {}
+
+    confParser::confParser(const std::string& configFile) : serverConfig(new ServerConfig) {
+        parseConfigFile(configFile);
+    }
+
+    confParser::confParser(const confParser& other) : serverConfig(new ServerConfig(*other.serverConfig)) {}
+
+    confParser::~confParser() {
+        delete serverConfig;
+    }
+
+    void confParser::parseConfigFile(const std::string& configFile) {
+    }
+
+    void confParser::readConfigFile(int fd) {
+
+    }
+
+    void confParser::setServerConfigValue() {
+
+    }
+
+    void confParser::setLocationConfigValue() {
+    }
+
 } // confParser
