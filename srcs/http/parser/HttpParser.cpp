506ce77d0084d569e49f83caed05b9553c63005b
RowMax03 <51720224+RowMax03@users.noreply.github.com>
2024-05-30 18:47:31 +0200

    Refactor HttpParser to use _variables for private Attributes

--- srcs/http/parser/HttpParser.cpp
+++ srcs/http/parser/HttpParser.cpp
@@ -6,7 +6,7 @@
 /*   By: mreidenb <mreidenb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 16:10:46 by mreidenb          #+#    #+#             */
-/*   Updated: 2024/05/30 18:39:53 by mreidenb         ###   ########.fr       */
+/*   Updated: 2024/05/30 18:46:54 by mreidenb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,13 @@ void HttpParser::parse(const std::string& raw) {
 
 	std::getline(request, line);
 	std::istringstream firstLine(line);
-	firstLine >> method >> url >> version;
+	firstLine >> _method >> _url >> _version;
 
 	// Split the URL into path and query string
-	std::size_t queryPos = url.find('?');
+	std::size_t queryPos = _url.find('?');
 	if (queryPos != std::string::npos) {
-		queryString = url.substr(queryPos + 1);
-		url = url.substr(0, queryPos);
+		_queryString = _url.substr(queryPos + 1);
+		_url = _url.substr(0, queryPos);
 	}
 
 	while (std::getline(request, line) && line != "\r") {
@@ -33,48 +33,48 @@ void HttpParser::parse(const std::string& raw) {
 		std::getline(headerLine, key, ':');
 		std::string value;
 		std::getline(headerLine, value);
-		headers[key] = value.substr(1);
+		_headers[key] = value.substr(1);
 	}
-	if (headers.find("Content-Length") != headers.end()) {
-		contentLength = std::stoi(headers["Content-Length"]);
-		std::vector<char> bodyChars(contentLength);
-		request.read(&bodyChars[0], contentLength);
-		body.assign(bodyChars.begin(), bodyChars.end());
+	if (_headers.find("Content-Length") != _headers.end()) {
+		_contentLength = std::stoi(_headers["Content-Length"]);
+		std::vector<char> _bodyChars(_contentLength);
+		request.read(&_bodyChars[0], _contentLength);
+		_body.assign(_bodyChars.begin(), _bodyChars.end());
 	}
 	else
-		contentLength = 0;
+		_contentLength = 0;
 }
 
 //envp needs to be null-terminated and freed after use
 void HttpParser::toCgiEnv(char **envp) const {
-	envp[0] = strdup(("REQUEST_METHOD=" + method).c_str());
+	envp[0] = strdup(("REQUEST_METHOD=" + _method).c_str());
 	//maybe needs a getter function later depending on implementation
-	envp[1] = strdup(("SCRIPT_NAME=" + scriptName).c_str());
-	envp[2] = strdup(("SERVER_PROTOCOL=" + version).c_str());
-	envp[3] = strdup(("CONTENT_LENGTH=" + std::to_string(contentLength)).c_str());
-	envp[4] = strdup(("QUERY_STRING=" + queryString).c_str());
+	envp[1] = strdup(("SCRIPT_NAME=" + _scriptName).c_str());
+	envp[2] = strdup(("SERVER_PROTOCOL=" + _version).c_str());
+	envp[3] = strdup(("CONTENT_LENGTH=" + std::to_string(_contentLength)).c_str());
+	envp[4] = strdup(("QUERY_STRING=" + _queryString).c_str());
 	//PATH_INFO is the part of the URL after the script name before the query string (if any)
 	//Also maybe needs a getter function later depending on implementation
-	envp[5] = strdup(("PATH_INFO=" + pathInfo).c_str());
+	envp[5] = strdup(("PATH_INFO=" + _pathInfo).c_str());
 
 }
 
 void HttpParser::parseUrl() {
-	std::size_t queryPos = url.find('?');
-	std::string path = url.substr(0, queryPos);
+	std::size_t queryPos = _url.find('?');
+	std::string path = _url.substr(0, queryPos);
 	std::size_t scriptPos = path.find("/cgi-bin/");
 	if (scriptPos != std::string::npos) {
-		scriptName = path.substr(scriptPos);
-		std::size_t pathInfoPos = scriptName.find('/');
-		if (pathInfoPos != std::string::npos) {
-			pathInfo = scriptName.substr(pathInfoPos);
-			scriptName = scriptName.substr(0, pathInfoPos);
+		_scriptName = path.substr(scriptPos);
+		std::size_t _pathInfoPos = _scriptName.find('/');
+		if (_pathInfoPos != std::string::npos) {
+			_pathInfo = _scriptName.substr(_pathInfoPos);
+			_scriptName = _scriptName.substr(0, _pathInfoPos);
 		}
 	}
 }
 
-std::string HttpParser::getMethod() const { return method; }
-std::string HttpParser::getUrl() const { return url; }
-std::string HttpParser::getVersion() const { return version; }
-std::string HttpParser::getBody() const { return body; }
-std::map<std::string, std::string> HttpParser::getHeaders() const { return headers; }
+std::string HttpParser::getMethod() const { return _method; }
+std::string HttpParser::getUrl() const { return _url; }
+std::string HttpParser::getVersion() const { return _version; }
+std::string HttpParser::getBody() const { return _body; }
+std::map<std::string, std::string> HttpParser::getHeaders() const { return _headers; }
--- srcs/http/parser/HttpParser.hpp
+++ srcs/http/parser/HttpParser.hpp
@@ -6,7 +6,7 @@
 /*   By: mreidenb <mreidenb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 15:56:39 by mreidenb          #+#    #+#             */
-/*   Updated: 2024/05/30 18:30:28 by mreidenb         ###   ########.fr       */
+/*   Updated: 2024/05/30 18:46:50 by mreidenb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,15 @@
 class HttpParser
 {
 private:
-	std::map<std::string, std::string> headers;
-	std::string method;
-	std::string url;
-	std::string scriptName;
-	std::string pathInfo;
-	std::string queryString;
-	std::string version;
-	std::string body;
-	int contentLength;
+	std::map<std::string, std::string> _headers;
+	std::string _method;
+	std::string _url;
+	std::string _scriptName;
+	std::string _pathInfo;
+	std::string _queryString;
+	std::string _version;
+	std::string _body;
+	int _contentLength;
 	void parseUrl();
 
 public:
