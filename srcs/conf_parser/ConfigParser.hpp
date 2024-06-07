4f44c8876269a8dfdc6c384e32711d8b77ba421e
nscheefe <nils.scheefe@gmail.com>
2024-06-07 18:51:21 +0200

    [Parser / Base] Update naming and variables from Parser, Reformat

--- srcs/conf_parser/ConfigParser.hpp
+++ srcs/conf_parser/ConfigParser.hpp
@@ -1,8 +1,8 @@
 // conf_parser.hpp
 #pragma once
 
-#include "server_conf.hpp"
-#include "location_conf.hpp"
+#include "ServerConfig.hpp"
+#include "LocationConfig.hpp"
 #include <map>
 #include <fstream>
 #include <cstring>
@@ -12,32 +12,33 @@
 #include <cstdlib>
 #include <sstream>
 
-namespace confParser {
-    class confParser {
+namespace Config {
+    class Parser {
+    private:
+
+        Server *_server;
+
+        Location *_location;
     public:
-        confParser(const std::string &configFile);
+        Parser(const std::string &configFile);
 
-        confParser(const confParser &other);
+        Parser(const Parser &other);
 
-        confParser &operator=(const confParser &other);
+        Parser &operator=(const Parser &other);
 
-        ~confParser();
+        ~Parser();
 
-        ServerConf *serverConfig;
 
-        LocationConfig *locationConfig;
+        std::vector<Server> servers;
 
     private:
         std::string removeLeadingSpaces(const std::string &str);
 
         void parseConfigFile(const std::string &configFile);
 
-        void readConfigFile(std::ifstream& file);
+        void readConfigFile(std::ifstream &file);
 
         std::vector <std::string> tokenize(const std::string &str, const std::string &delim);
 
-        void setServerConfigValue(std::vector <std::vector<std::string> > &serverConfVec);
-
-        void setLocationConfigValue(std::vector <std::vector<std::string> > &locationConfVec);
     };
 }
