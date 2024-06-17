0c0e2a7c3a0529ede6103f57f425358ff51652a6
RowMax03 <51720224+RowMax03@users.noreply.github.com>
2024-06-17 12:56:35 +0200

    chore: Refactor Server constructor to use Config::Parser and support multiple servers

--- srcs/server/Server.hpp
+++ srcs/server/Server.hpp
@@ -6,7 +6,7 @@
 /*   By: mreidenb <mreidenb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/20 18:56:15 by mreidenb          #+#    #+#             */
-/*   Updated: 2024/05/21 17:43:06 by mreidenb         ###   ########.fr       */
+/*   Updated: 2024/06/17 12:55:52 by mreidenb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,9 @@ struct pollfd;
 class Server
 {
 private:
-	ServerSocket server;
+	Config::Parser _conf;
+	std::vector<ServerSocket> _servers;
+	size_t _server_count;
 	std::vector<ClientSocket*> _clients;
 	std::vector<pollfd> _pollfds;
 	void removeClient(size_t i);
@@ -32,16 +34,20 @@ private:
 	void pollin(size_t i);
 	void pollout(size_t i);
 public:
-	Server(const Config::Server conf);
+	Server(const Config::Parser conf);
 	~Server();
 	int Start();
 };
 
 // * Will be changed to read from Config later *
 // is implmented but have to talk of what to use where and how to is in my mind server name the domain but here it is int but a domain is string in my mind
-Server::Server(const Config::Server conf) : server(AF_INET, SOCK_STREAM, 0, INADDR_ANY, conf.listen)
+Server::Server(const Config::Parser conf) : _conf(conf) , _server_count(conf.servers.size())
 {
-	this->_pollfds.push_back((pollfd){server.getFD(), POLLIN, 0});
+	for (size_t i = 0; i < _server_count; i++) {
+		_servers.push_back(ServerSocket(AF_INET, SOCK_STREAM, 0, INADDR_ANY, conf.servers[i].listen));
+		_servers[i].listen_socket(5);
+		_pollfds.push_back((pollfd){_servers[i].getFD(), POLLIN, 0});
+	}
 }
 
 Server::~Server()
@@ -66,7 +72,6 @@ void Server::removeClient(size_t i)
 
 int Server::Start()
 {
-	server.listen_socket(5);
 	while (1) {
 		if (poll(_pollfds.data(), _pollfds.size(), 100000) < 0) {
 			perror("poll failed");
@@ -76,8 +81,8 @@ int Server::Start()
 			if (_pollfds[i].revents == 0) {
 				continue;
 			}
-			if (!i && _pollfds[i].revents & POLLIN) {
-				addClient(server.accept_socket());
+			if (i < _server_count && _pollfds[i].revents & POLLIN) {
+				addClient(_servers[i].accept_socket());
 			}
 			else if (_pollfds[i].revents == POLLIN) { // ClientSocket is ready to read
 				pollin(i);
