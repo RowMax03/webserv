0de910d2acc9a946ecd476315272d12d4cdb306c
RowMax03 <51720224+RowMax03@users.noreply.github.com>
2024-06-17 18:11:13 +0200

    chore: Update CGI constructor to include documentRoot parameter

--- srcs/cgi/CGI.cpp
+++ srcs/cgi/CGI.cpp
@@ -6,7 +6,7 @@
 /*   By: mreidenb <mreidenb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/31 23:00:40 by mreidenb          #+#    #+#             */
-/*   Updated: 2024/06/17 13:45:03 by mreidenb         ###   ########.fr       */
+/*   Updated: 2024/06/17 18:09:53 by mreidenb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
  * @param request The HttpParser object that contains the request
  * @param config The Config object that containts the cgi config
  */
-CGI::CGI(const HttpParser &request) : _request(request) {
+CGI::CGI(const HttpParser &request, const std::string &documentRoot) : _request(request), _documentRoot(documentRoot) {
 	toCharArr(request.toCgiEnv());
 }
 
@@ -62,9 +62,9 @@ void CGI::deleteEnv() {
  */
 std::string CGI::run() {
 	//document root, get from config later
-	std::string documentRoot = "/Users/max/Projekte/webserv";
+	//std::string documentRoot = "/Users/max/Projekte/webserv";
 	// check if we can open the file
-	checkRigths(documentRoot + _request.getPath());
+	checkRigths(_documentRoot + _request.getPath());
 	int inputPipe[2];
 	int outputPipe[2];
 
@@ -76,7 +76,7 @@ std::string CGI::run() {
 	if (pid == -1)
 		throw std::runtime_error("fork failed");
 	if (pid == 0)
-		handleChildProcess(inputPipe, outputPipe, documentRoot);
+		handleChildProcess(inputPipe, outputPipe, _documentRoot);
 	else
 		return handleParentProcess(inputPipe, outputPipe, pid);
 	return "";
