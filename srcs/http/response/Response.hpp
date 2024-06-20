6e99d6459c43fdacb42d955ea23ab73d93f204bf
nscheefe <nils.scheefe@gmail.com>
2024-06-20 17:42:44 +0200

    [Response] add init function and seralization function

--- srcs/http/response/Response.hpp
+++ srcs/http/response/Response.hpp
@@ -1,4 +1,41 @@
 #include <string>
+#include "ResponseBody.hpp"
+#include "ResponseHead.hpp"
 
 class Response {
+
+private:
+    HttpParser _parser;
+    Config _config;
+    ResponseHead responseHead;
+    ResponseBody responseBody;
+
+public:
+    Response(const HttpParser& parser, const Config& config) : _parser(parser), _config(config), responseHead(parser, config), responseBody(parser, config) {}
+
+    Response(const Response& other) : _parser(other._parser), _config(other._config), responseHead(other.responseHead), responseBody(other.responseBody) {}
+
+    Response& operator=(const Response& other) {
+        if (this != &other) {
+            _parser = other._parser;
+            _config = other._config;
+            responseHead = other.responseHead;
+            responseBody = other.responseBody;
+        }
+        return *this;
+    }
+
+    ~Response() {}
+
+    void init() {
+        responseHead.init();
+        responseBody.init();
+    }
+
+    std::string serialize() {
+        std::string response = responseHead.serialize();
+        response += responseBody.serialize();
+
+        return response;
+    }
 };
