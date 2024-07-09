89053ae252cf207af9e8f83fc0554f9c49eff9b6
nscheefe <nils.scheefe@gmail.com>
2024-07-09 23:13:24 +0200

    [config_parser] Update locationConfig add value allowCgi

--- srcs/conf_parser/LocationConfig.hpp
+++ srcs/conf_parser/LocationConfig.hpp
@@ -9,12 +9,12 @@
 namespace Config {
     class Location : public confBase {
     public:
-        Location() : path(""), redirect_status(0), redirect_url(""), root(""), autoindex(false), index("") {}
+        Location() : path(""), redirect_status(0), redirect_url(""), root(""), autoindex(false), allowCgi(false), index("") {}
 
         Location(const Location &other) : confBase(other), path(other.path),
                                           redirect_status(other.redirect_status),
                                           redirect_url(other.redirect_url), root(other.root),
-                                          autoindex(other.autoindex), index(other.index) {
+                                          autoindex(other.autoindex), allowCgi(other.allowCgi), index(other.index) {
             methods = other.methods;
         }
 
@@ -26,6 +26,7 @@ namespace Config {
                 redirect_url = other.redirect_url;
                 root = other.root;
                 autoindex = other.autoindex;
+                allowCgi = other.allowCgi;
                 index = other.index;
             }
             return *this;
@@ -60,15 +61,20 @@ namespace Config {
                 } else if (key == "root") {
                     root = "/" + removeLeadingSlash(removeTrailingSlash(configValues[1])) + "/";
                 } else if (key == "autoindex") {
-                    autoindex = (configValues[1] == "on") ? true : ((configValues[1] == "off") ? false
-                                                                : throw std::invalid_argument("Invalid autoindex value"));
-                } else if (key == "index") {
+                    autoindex = (configValues[1] == "on");
+                } else if (key == "Cgi"){
+                    std::cout << "isCgi: " << configValues[1] << std::endl;
+                    allowCgi = (configValues[1] == "on");
+
+                    std::cout << "isCgi: " << allowCgi << std::endl;
+
+                }else if (key == "index") {
                     index = "/" + removeLeadingSlash(configValues[1]);
                 } else if (key == "allow") {
                     for (size_t i = 1; i < configValues.size(); i++) {
                         methods.push_back(configValues[i]);
                     }
-                } else {
+                } else{
                     throw std::invalid_argument("Invalid key");
                 }
             } else
@@ -81,7 +87,7 @@ namespace Config {
             std::cout << "\t\tLocation: \n\t\t\tpath=" << path << "\n\t\t\tmethods=" << (methods.size() > 0 ? methods[0] : "")
                       << " " << (methods.size() > 1 ? methods[1] : "") << " " << (methods.size() > 2 ? methods[2] : "") << " " << (methods.size() > 3 ? methods[3] : "") << "\n\t\t\tredirect_status=" << redirect_status
                       << "\n\t\t\tredirect_url=" << redirect_url << "\n\t\t\troot=" << root
-                      << "\n\t\t\tautoindex=" << autoindex << "\n\t\t\tindex=" << index << std::endl;
+                      << "\n\t\t\tautoindex=" << (autoindex ? "on":"off") << "\n\t\t\tindex=" << index << "\n\t\t\tCgi="<< (allowCgi ? "on" : "off" )<< std::endl;
         }
 
         void validate() {
@@ -93,6 +99,7 @@ namespace Config {
         std::string redirect_url;
         std::string root;
         bool autoindex;
+        bool allowCgi;
         std::string index;
     };
 }
